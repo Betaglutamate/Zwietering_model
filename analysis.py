fc1533d514b9a26a6a448a6123ef7ec03fa582a4
Mark Zurbruegg <39518170+Betaglutamate@users.noreply.github.com>
2021-02-03 17:12:11 +0000

    extratc data and put into pandas df

---
+++ analysis.py
@@ -0,0 +1,70 @@
+import numpy
+import pandas as pd
+import numpy as np
+import csv
+
+from pandas.core.indexes.base import Index
+
+
+# so first we want to open and analyse a single file
+
+inputDf = pd.read_excel(
+    "Data/202009_M63GluCaa_Sucrose_37c/run1/20200929_sucrose_m63caaglu_30c.xlsx")
+
+
+inputDfOd = inputDf[45:143].transpose()
+inputDfGfp = inputDf[146:244].transpose()
+
+
+# Now I want to set the Index as Time
+
+
+inputDfOd.reset_index(inplace=True, drop=True)
+inputDfGfp.reset_index(inplace=True, drop=True)
+
+colsToDrop = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 25,
+ 26, 37, 38, 49, 50, 61, 62, 73, 74, 85, 86, 87,
+ 88, 89, 90, 91, 92, 93, 94, 95, 96, 97]
+
+
+inputDfOd.drop(inputDfOd.columns[colsToDrop], axis=1, inplace=True)
+inputDfGfp.drop(inputDfGfp.columns[colsToDrop], axis=1, inplace=True)
+
+
+#now drop the first row as it contains our headers and drop any NA from empty data
+inputDfOd = inputDfOd.drop(inputDfOd.index[0]).dropna()
+inputDfGfp = inputDfGfp.drop(inputDfGfp.index[0]).dropna()
+
+
+
+# Now we need to name the columns correctly
+# Here I call the column names saved in a csv and create a list from them named colNames
+
+with open('Data/01_helper_data/platereaderLayout.csv', newline='') as f:
+    reader = csv.reader(f)
+    data = list(reader)
+
+colNames = data[0]
+
+inputDfOd.rename(columns=dict(zip(inputDfOd.columns, colNames)),  inplace=True)
+inputDfGfp.rename(columns=dict(zip(inputDfGfp.columns, colNames)),  inplace=True)
+
+
+#now find the Time in minutes
+newTime = [round(x*(7.6),2) for x in range(0, len(inputDfOd))]
+inputDfOd["Time (min)"] = newTime
+inputDfGfp["Time (min)"] = newTime
+
+# now I want to subtract the average of the first row from all values in the dataframe
+# OdMeanBg = inputDfOd.loc[1][2:61].mean()
+# GfpMeanBg = inputDfGfp.loc[1][2:61].mean()
+
+# inputDfOd.loc[:, inputDfOd.columns != 'Time (min)'] = inputDfOd.loc[:, inputDfOd.columns != 'Time (min)']- OdMeanBg
+# inputDfGfp.loc[:, inputDfGfp.columns != 'Time (min)'] = inputDfGfp.loc[:, inputDfGfp.columns != 'Time (min)']- GfpMeanBg
+
+firstRowOd = inputDfOd.iloc[[0]].values[0]
+finalOd = inputDfOd.apply(lambda row: row - firstRowOd, axis=1)
+
+firstRowGfp = inputDfGfp.iloc[[0]].values[0]
+finalGfp = inputDfGfp.apply(lambda row: row - firstRowGfp, axis=1)
+
