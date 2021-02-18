69ab4d00425a504dd0220e085aafc6c6f8523d47
Mark Zurbruegg <39518170+Betaglutamate@users.noreply.github.com>
2021-02-18 16:39:58 +0000

    updateded calculation functions

--- analysis.py
+++ analysis.py
@@ -1,7 +1,8 @@
-from analysisFunctions import normalize_plate, align_df, generate_plots, calculate_regression
+from analysisFunctions import HelperFunctions as hf
 import pandas as pd
 import numpy as np
 from scipy import stats
+import plotnine as gg
 
 files_to_analyze = []
 
@@ -11,99 +12,53 @@ for root, dirs, files in os.walk("Data"):
         if filename.endswith(".xlsx"):
             files_to_analyze.append({"root": root, "filename": filename})
 
-def analyze_plate(file, **kwargs):
-    keywords = kwargs
 
-    root = file['root']
-    filename = file['filename']
-
-    plate_path = os.path.join(root, filename)
-    plate_normalized = normalize_plate(plate_path)
-
-    # Align the dataframe to a specific value for individual values
-
-    a = plate_normalized['OD'].melt(id_vars="Time (min)")
-    a = a.rename(columns = dict(zip(a.columns, ["Time", "variable", "OD"])))
-
-    b = plate_normalized['GFP'].melt(id_vars="Time (min)")
-    b = b.rename(columns = dict(zip(b.columns, ["Time", "variable", "GFP"])))
-
-    merged = a.merge(b)
-
-    #OK its in the long format now to align it to OD
-
-    split = merged.groupby('variable')    
-    split_df = [split.get_group(x) for x in split.groups]
-
-    aligned_df_long = []
-
-    for df in split_df:
-        aligned_df = align_df(df)
-        aligned_df_long.append(aligned_df)
-
-    aligned_df_long = pd.concat(aligned_df_long)    
-    aligned_df_long['Group'] = aligned_df_long['variable'].apply(lambda x: x[0:7])
-    aligned_df_long['GFP/OD'] = aligned_df_long['GFP'] / aligned_df_long['OD']
-    aligned_df_long['log(OD)'] = np.log(aligned_df_long['OD'])
-
-    if keywords.get('plot'):
-        print("generating plots")
-        generate_plots(aligned_df_long, root)
-
-    aligned_df_long = calculate_regression(aligned_df_long)
-
-    return aligned_df_long
+test = hf.analyze_plate(files_to_analyze[0], plot = False)
 
 
-test = analyze_plate(files_to_analyze[0], plot = False)
+max_growth = test.groupby('variable').apply(hf.calculate_max_growth_rate)    
 
-split = test.groupby('variable')    
+def calculate_max_growth_rate(df):
+        df['smoothed_GR'] = df['GrowthRate'].rolling(8, win_type = "gaussian").mean(std=1)
+        df['smoothed_std'] = (df['GrowthRate'].rolling(8).std())
+        df['adjusted'] =  df['smoothed_std']/df['smoothed_GR']
+        run_length, run_index = hf.longest_run(df['smoothed_GR'].values[0:100])
+        max_growth_rate = np.mean(df['smoothed_GR'].values[run_index: run_index+run_length])
+
+        return df
+
+
+testing = calculate_max_growth_rate(split_df[59])
+
+
+new_test = test.groupby('variable').apply(calculate_regression_growth_rate).reset_index(drop=True)
+
+split = new_test.groupby('variable')    
 split_df = [split.get_group(x) for x in split.groups]
 
-(
-    gg.ggplot(split_df[8][20:]) +
-    gg.aes(x="Time", y= "GrowthRate") +
-    gg.geom_point()
-
-)
-
-a = split_df[8]['GrowthRate'].values
-
-diff_length = []
-
-for num in range(len(a)-8):
-    val1 = a[num]
-    val2 = a[num+2]
-    val3 = a[num+3]
-    val4 = a[num+4]
-    val5 = a[num+5]
-    val6 = a[num+6]
-    val7 = a[num+7]
-    val8 = a[num+8]
-
-    dif1 = (val1-val2)**2
-    dif2 = (val1-val3)**2
-    dif3 = (val1-val4)**2
-    dif4 = (val1-val5)**2
-    dif5 = (val1-val6)**2
-    dif6 = (val1-val7)**2
-    dif7 = (val1-val8)**2
-    final_dif = dif1 + dif2 + dif3 + dif4 + dif5 + dif6 + dif7
-    diff_length.append(final_dif)
-
-diff_length = pd.DataFrame({"diff": diff_length})
-
-final = pd.concat([split_df[8],diff_length], axis=1)
-final.loc[final['diff'] < 0.0005]
-
 
 (
-    gg.ggplot(final) +
-    gg.aes(x="Time", y= "diff") +
+    gg.ggplot(testing) +
+    gg.aes(x= "Time", y = "adjusted") +
     gg.geom_point()+
-    gg.ylim(0, 0.0025)
-
+    gg.xlim(0,1000)+
+    gg.ylim(0,0.5)
 )
+
+#best so far bohmaN
+
+(
+    gg.ggplot(split_df[48]) +
+    gg.aes(x= "Time", y = "GrowthRate") +
+    gg.geom_point()+
+    gg.xlim(0, 1000)+
+    gg.geom_hline(yintercept = 0.20)
+)
+
+# meanofstdev = test['stdev'].rolling(16, win_type="gaussian").mean(std=1)#16 for 2 hours
+# meanofstdev.rename("meanstdev", inplace = True)
+
+
 
 import time
 
@@ -136,7 +91,6 @@ calculate_regression(test)
 end = time.time()
 
 print(end-start)
-
 
 
 
--- analysisFunctions.py
+++ analysisFunctions.py
@@ -5,121 +5,112 @@ import csv
 import plotnine as gg
 from scipy import stats
 
+class HelperFunctions:
+
+    def __init__(self):
+        pass
+
+    def normalize_plate(pathToExcel):
+        """
+        This function takes in the pathToExcel file and outputs the normalized GFP and OD values as a dict
+        """
+        # so first we want to open and analyse a single file
+
+        input_df = pd.read_excel(pathToExcel)
+
+
+        input_df_od = input_df[45:143].transpose()
+        input_df_gfp = input_df[146:244].transpose()
+
+
+        # Now I want to set the Index as Time
+
+
+        input_df_od.reset_index(inplace=True, drop=True)
+        input_df_gfp.reset_index(inplace=True, drop=True)
+
+        cols_to_drop = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 25,
+        26, 37, 38, 49, 50, 61, 62, 73, 74, 85, 86, 87,
+        88, 89, 90, 91, 92, 93, 94, 95, 96, 97]
+
+
+        input_df_od.drop(input_df_od.columns[cols_to_drop], axis=1, inplace=True)
+        input_df_gfp.drop(input_df_gfp.columns[cols_to_drop], axis=1, inplace=True)
+
+
+        #now drop the first row as it contains our headers and drop any NA from empty data
+        input_df_od = input_df_od.drop(input_df_od.index[0]).dropna()
+        input_df_gfp = input_df_gfp.drop(input_df_gfp.index[0]).dropna()
 
 
 
-def normalize_plate(pathToExcel):
-    """
-    This function takes in the pathToExcel file and outputs the normalized GFP and OD values as a dict
-    """
-    # so first we want to open and analyse a single file
+        # Now we need to name the columns correctly
+        # Here I call the column names saved in a csv and create a list from them named colNames
 
-    input_df = pd.read_excel(pathToExcel)
+        with open('Data/01_helper_data/platereaderLayout.csv', newline='') as f:
+            reader = csv.reader(f)
+            data = list(reader)
 
+        col_names = data[0]
 
-    input_df_od = input_df[45:143].transpose()
-    input_df_gfp = input_df[146:244].transpose()
-
-
-    # Now I want to set the Index as Time
+        input_df_od.rename(columns=dict(zip(input_df_od.columns, col_names)),  inplace=True)
+        input_df_gfp.rename(columns=dict(zip(input_df_gfp.columns, col_names)),  inplace=True)
 
 
-    input_df_od.reset_index(inplace=True, drop=True)
-    input_df_gfp.reset_index(inplace=True, drop=True)
+        #now find the Time in minutes
+        new_time = [round(x*(7.6),2) for x in range(0, len(input_df_od))]
+        input_df_od["Time (min)"] = new_time
+        input_df_gfp["Time (min)"] = new_time
 
-    cols_to_drop = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 25,
-    26, 37, 38, 49, 50, 61, 62, 73, 74, 85, 86, 87,
-    88, 89, 90, 91, 92, 93, 94, 95, 96, 97]
+        # now I want to subtract the average of the first row from all values in the dataframe
 
+        firstRowOd = input_df_od.iloc[[0]].values[0]
+        finalOd = input_df_od.apply(lambda row: row - firstRowOd, axis=1)
 
-    input_df_od.drop(input_df_od.columns[cols_to_drop], axis=1, inplace=True)
-    input_df_gfp.drop(input_df_gfp.columns[cols_to_drop], axis=1, inplace=True)
+        firstRowGfp = input_df_gfp.iloc[[0]].values[0]
+        finalGfp = input_df_gfp.apply(lambda row: row - firstRowGfp, axis=1)
 
-
-    #now drop the first row as it contains our headers and drop any NA from empty data
-    input_df_od = input_df_od.drop(input_df_od.index[0]).dropna()
-    input_df_gfp = input_df_gfp.drop(input_df_gfp.index[0]).dropna()
-
-
-
-    # Now we need to name the columns correctly
-    # Here I call the column names saved in a csv and create a list from them named colNames
-
-    with open('Data/01_helper_data/platereaderLayout.csv', newline='') as f:
-        reader = csv.reader(f)
-        data = list(reader)
-
-    col_names = data[0]
-
-    input_df_od.rename(columns=dict(zip(input_df_od.columns, col_names)),  inplace=True)
-    input_df_gfp.rename(columns=dict(zip(input_df_gfp.columns, col_names)),  inplace=True)
-
-
-    #now find the Time in minutes
-    new_time = [round(x*(7.6),2) for x in range(0, len(input_df_od))]
-    input_df_od["Time (min)"] = new_time
-    input_df_gfp["Time (min)"] = new_time
-
-    # now I want to subtract the average of the first row from all values in the dataframe
-
-    firstRowOd = input_df_od.iloc[[0]].values[0]
-    finalOd = input_df_od.apply(lambda row: row - firstRowOd, axis=1)
-
-    firstRowGfp = input_df_gfp.iloc[[0]].values[0]
-    finalGfp = input_df_gfp.apply(lambda row: row - firstRowGfp, axis=1)
-
-    return {"OD": finalOd, "GFP": finalGfp}
+        return {"OD": finalOd, "GFP": finalGfp}
 
 
 
 
-def align_df(split_df, **kwargs):
-    """
-    This function takes a single experiment and aligns it to > stDev*5
-    or you can pass in a kw od = num to set alignment
-    """
+    def align_df(split_df, **kwargs):
+        """
+        This function takes a single experiment and aligns it to > stDev*5
+        or you can pass in a kw od = num to set alignment
+        """
 
-    new_time = split_df["Time"].values
-    st_dev =  np.std(split_df['OD'].iloc[0:10])
-    od_filter_value = st_dev*5
-    if kwargs:
-        od_filter_value = kwargs.get('od')
-    filtered_new = split_df.loc[split_df['OD'] > od_filter_value].reset_index(drop = True)
-    filtered_new["Time"] = new_time[0:len(filtered_new)]
+        new_time = split_df["Time"].values
+        st_dev =  np.std(split_df['OD'].iloc[0:10])
+        od_filter_value = st_dev*5
+        if kwargs:
+            od_filter_value = kwargs.get('od')
+        filtered_new = split_df.loc[split_df['OD'] > od_filter_value].reset_index(drop = True)
+        filtered_new["Time"] = new_time[0:len(filtered_new)]
 
-    return filtered_new
+        return filtered_new
 
-def generate_plots(long_df, directory):
-    """
-    This function takes in a long Df it should then output a graph for every
-    group
-    """
+    def generate_plots(long_df, directory):
+        """
+        This function takes in a long Df it should then output a graph for every
+        group
+        """
 
-    split = long_df.groupby('Group')    
-    split_df = [split.get_group(x) for x in split.groups]
+        split = long_df.groupby('Group')    
+        split_df = [split.get_group(x) for x in split.groups]
 
-    for num, df in enumerate(split_df):
-        group_plot = (
-            gg.ggplot(df)+
-            gg.aes(x='Time', y='OD', color='variable')+
-            gg.geom_point()+
-            gg.ggtitle(df['Group'].values[0])
-        )
-        saveString = f"test{num}.png"
-        gg.ggsave(group_plot, os.path.join(directory, saveString))
+        for num, df in enumerate(split_df):
+            group_plot = (
+                gg.ggplot(df)+
+                gg.aes(x='Time', y='OD', color='variable')+
+                gg.geom_point()+
+                gg.ggtitle(df['Group'].values[0])
+            )
+            saveString = f"test{num}.png"
+            gg.ggsave(group_plot, os.path.join(directory, saveString))
 
-
-def calculate_regression(long_df):
-    """
-    This function will take the long_df and calculate a rolling window
-    fit for growth rate
-    """
-    window_size = 8
-    split = long_df.groupby('variable')    
-    split_df = [split.get_group(x) for x in split.groups]
-    long_df = []
-
-    for df in split_df:
+    def calculate_regression(df, window_size = 8):
         regress_list = []
         for length in range(len(df)):
             res = stats.linregress(
@@ -129,8 +120,72 @@ def calculate_regression(long_df):
             regress_list.append((res.slope*60))
         
         df['GrowthRate'] = regress_list
-        long_df.append(df)
-    
-    long_df = pd.concat(long_df)
-    return long_df
+        return df
 
+    def calculate_regression_long(long_df):
+        """
+        This function will take the long_df and calculate a rolling window
+        fit for growth rate
+        """
+        long_df = long_df.groupby('variable').apply(HelperFunctions.calculate_regression).reset_index(drop = True)   
+        return long_df
+
+
+    def longest_run(myList):
+        prev = 0
+        size = 0
+        max_size = 0
+        max_list = []
+
+        for num, i in enumerate(myList):
+            if i > (prev *0.99) and i < (prev * 1.01):
+                size += 1
+                if size > max_size:
+                    max_size = size 
+                    max_list.append(num-1)
+            else:
+                size = 0
+            prev = i
+        return max_size+1, (max(max_list)-max_size+1)
+
+    def analyze_plate(file, **kwargs):
+        keywords = kwargs
+
+        root = file['root']
+        filename = file['filename']
+
+        plate_path = os.path.join(root, filename)
+        plate_normalized = HelperFunctions.normalize_plate(plate_path)
+
+        # Align the dataframe to a specific value for individual values
+
+        a = plate_normalized['OD'].melt(id_vars="Time (min)")
+        a = a.rename(columns = dict(zip(a.columns, ["Time", "variable", "OD"])))
+
+        b = plate_normalized['GFP'].melt(id_vars="Time (min)")
+        b = b.rename(columns = dict(zip(b.columns, ["Time", "variable", "GFP"])))
+
+        merged = a.merge(b)
+
+        #OK its in the long format now to align it to OD
+
+        aligned_df_long = merged.groupby('variable').apply(HelperFunctions.align_df).reset_index(drop=True) 
+
+        aligned_df_long['Group'] = aligned_df_long['variable'].apply(lambda x: x[0:7])
+        aligned_df_long['GFP/OD'] = aligned_df_long['GFP'] / aligned_df_long['OD']
+        aligned_df_long['log(OD)'] = np.log(aligned_df_long['OD'])
+
+        if keywords.get('plot'):
+            print("generating plots")
+            HelperFunctions.generate_plots(aligned_df_long, root)
+
+        aligned_df_long = HelperFunctions.calculate_regression(aligned_df_long)
+
+        return aligned_df_long
+
+    def calculate_max_growth_rate(df):
+        df['smoothed_GR'] = df['GrowthRate'].rolling(8, win_type="barthann").mean()
+        run_length, run_index = HelperFunctions.longest_run(df['smoothed_GR'].values[0:100])
+        max_growth_rate = np.mean(df['smoothed_GR'].values[run_index: run_index+run_length])
+
+        return max_growth_rate
