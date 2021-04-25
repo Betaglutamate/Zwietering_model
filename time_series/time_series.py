fba359a15f96cef178333d2c4e3e049565941cc6
Mark Zurbruegg <39518170+Betaglutamate@users.noreply.github.com>
2021-04-25 15:34:39 +0100

    implmented working methods

--- time_series/time_series.py
+++ time_series/time_series.py
@@ -8,10 +8,6 @@ from sktime.transformations.panel.rocket import Rocket
 
 main_df = pd.read_csv('experiment1_df.csv').drop(columns=['Unnamed: 0'])
 test_df = pd.read_csv('experiment8_df.csv').drop(columns=['Unnamed: 0'])
-
-
-#you want to create a list of wavelets for each variable
-
 length_window = 20
 
 def create_subcurve(single_variable):
@@ -32,83 +28,115 @@ def create_subcurve(single_variable):
 
     return X_values, y_values, y_growth_values, time_values
 
+def test_classifier(test_df):
+    X_list = []
+    y_list = []
+    y_growth_list = []
+    time_list = []
 
-X_list = []
-y_list = []
-y_growth_list = []
-time_list = []
+    for name, variable in test_df.groupby('variable'):
+        X, y, y_growth, time = create_subcurve(variable)
+        X_list.append(X)
+        y_list.append(y)
+        time_list.append(time)
+        y_growth_list.append(y_growth)
 
-for name, variable in main_df.groupby('variable'):
-    X, y, y_growth, time = create_subcurve(variable)
-    X_list.append(X)
-    y_list.append(y)
-    time_list.append(time)
-    y_growth_list.append(y_growth)
+    # now try for the entire data
+    flat_x = [item for sublist in X_list for item in sublist]
+    flat_y = [item for sublist in y_list for item in sublist]
+    flat_growth = [item for sublist in y_growth_list for item in sublist]
+
+    xData = np.array(pd.Series(flat_x))
+    growthData = np.array(pd.Series(flat_growth))
+    yData = np.array(flat_y)
+
+    xData_small = xData[1::3]
+    yData_small = yData[1::3]
+    growthData_small = growthData[1::3]
+
+    df_test_fit_x = pd.DataFrame({"dim_0": xData_small, "dim_1" : growthData_small})
+    df_test_fit_y = pd.DataFrame({"dim_0": yData_small})
+
+    return df_test_fit_x, df_test_fit_y
+
+def generate_classifier(train_df, test_df):
+
+#you want to create a list of wavelets for each variable
+    X_list = []
+    y_list = []
+    y_growth_list = []
+    time_list = []
+
+    for name, variable in train_df.groupby('variable'):
+        X, y, y_growth, time = create_subcurve(variable)
+        X_list.append(X)
+        y_list.append(y)
+        time_list.append(time)
+        y_growth_list.append(y_growth)
+
+    # now try for the entire data
+    flat_x = [item for sublist in X_list for item in sublist]
+    flat_y = [item for sublist in y_list for item in sublist]
+    flat_growth = [item for sublist in y_growth_list for item in sublist]
+
+    xData = np.array(pd.Series(flat_x))
+    growthData = np.array(pd.Series(flat_growth))
+    yData = np.array(flat_y)
+
+    xData_small = xData[1::3]
+    yData_small = yData[1::3]
+    growthData_small = growthData[1::3]
+
+    df_train_x = pd.DataFrame({"dim_0": xData_small, "dim_1" : growthData_small})
+
+    rocket = Rocket()  # by default, ROCKET uses 10,000 kernels
+    rocket.fit(df_train_x)
+    X_train_transform = rocket.transform(df_train_x)
+
+    classifier = RidgeClassifierCV(alphas=np.logspace(-3, 3, 10), normalize=True)
+    classifier.fit(X_train_transform, yData_small)
+
+    #Compare to another sample data here
+    df_test_fit_x, df_test_fit_y = test_classifier(test_df)
+
+    X_test_transform = rocket.transform(df_test_fit_x)
+
+    accuracy = classifier.score(X_test_transform, df_test_fit_y)
+    
+    return classifier
+    
+
+    print(accuracy)
 
 
- # now try for the entire data
-flat_x = [item for sublist in X_list for item in sublist]
-flat_y = [item for sublist in y_list for item in sublist]
-flat_growth = [item for sublist in y_growth_list for item in sublist]
-
-
-# next(flat_x)
-# next(flat_y)
-
-xData = np.array(pd.Series(flat_x))
-growthData = np.array(pd.Series(flat_growth))
-yData = np.array(flat_y)
-
-xData_small = xData[1::3]
-yData_small = yData[1::3]
-growthData_small = growthData[1::3]
-
-df_test_x = pd.DataFrame({"dim_0": xData_small, "dim_1" : growthData_small})
-df_test_y = pd.DataFrame({"dim_0": yData_small})
-
-rocket = Rocket()  # by default, ROCKET uses 10,000 kernels
-rocket.fit(df_test_x)
-X_train_transform = rocket.transform(df_test_x)
-
-classifier = RidgeClassifierCV(alphas=np.logspace(-3, 3, 10), normalize=True)
-classifier.fit(X_train_transform, yData_small)
+generate_classifier(main_df, test_df)
 
 
 #Here I will test the fit of my model
 
-x_fit_test = X_list[29]
-y_fit_test = y_list[29]
-y_growth_test =  y_growth_list[26]
 
-xTime = time_list[26]
+    #make values for plotting
+    # xOD = np.fromiter((x.values[0] for x in y_growth_test), float)
+    # transformed = classifier.predict(X_test_transform)
 
-df_test_fit_x = pd.DataFrame({"dim_0": x_fit_test, "dim_1" : y_growth_test})
-df_test_fit_y = pd.DataFrame({"dim_0": y_fit_test})
+    # sns.scatterplot(x=xTime, y=xOD, hue=transformed)
 
-X_test_transform = rocket.transform(df_test_fit_x)
+    # Ok no you want to split wt and GFP
 
-print(classifier.score(X_test_transform, df_test_fit_y))
+    xTest = X_list[0]
+    yTest = y_list[0]
 
-#make values for plotting
-xOD = np.fromiter((x.values[0] for x in y_growth_test), float)
-transformed = classifier.predict(X_test_transform)
+    df_test_x = pd.DataFrame({"dim_0": xTest})
+    df_test_y = pd.DataFrame({"dim_0": yTest})
 
-sns.scatterplot(x=xTime, y=xOD, hue=transformed)
+    rocket = Rocket()  # by default, ROCKET uses 10,000 kernels
+    rocket.fit(df_test_x)
+    X_train_transform = rocket.transform(df_test_x)
 
-# Ok no you want to split wt and GFP
+    classifier = RidgeClassifierCV(alphas=np.logspace(-3, 3, 10), normalize=True)
+    classifier.fit(X_train_transform, yTest)
 
-xTest = X_list[0]
-yTest = y_list[0]
-
-df_test_x = pd.DataFrame({"dim_0": xTest})
-df_test_y = pd.DataFrame({"dim_0": yTest})
-
-rocket = Rocket()  # by default, ROCKET uses 10,000 kernels
-rocket.fit(df_test_x)
-X_train_transform = rocket.transform(df_test_x)
-
-classifier = RidgeClassifierCV(alphas=np.logspace(-3, 3, 10), normalize=True)
-classifier.fit(X_train_transform, yTest)
+    
 
 ##here you are seeing if it works against new data
 
